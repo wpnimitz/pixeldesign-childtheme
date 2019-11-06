7ccb8ee8d56c661e2f011bd5761d8162590d86a6
wpnimitz <wpnimitz@gmail.com>
2019-11-06 21:30:02 +0800

    Update single-rental.php

--- single-rental.php
+++ single-rental.php
@@ -138,6 +138,8 @@ if($property_beds == "") {
 									</div>
 									
 									<div class="meta full">
+									<span class="propertyNumber"><?php echo $property_bedrooms ?></span>
+										<br>Bedroom<?php echo ($property_bedrooms > 1) ? 's' : '' ?>
 									<?php if(!empty($bedroom_label)) {
 										echo '<table id="bedrooms" class="bedroomsDetails" style="border:3px solid red;">';
 										for ($i=0; $i < count($bedroom_label); $i++) { 
