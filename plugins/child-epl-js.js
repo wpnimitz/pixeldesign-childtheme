d8513c9d841777707c64e9bc7995dbca8e4c30fc
dougintexas <doug@pixelprimedesign.com>
2019-11-06 20:11:05 -0600

    Update child-epl-js.js

--- plugins/child-epl-js.js
+++ plugins/child-epl-js.js
@@ -268,7 +268,7 @@ jQuery(document).ready(function( $ ) {
         allowtoLoad = false;
         if(maxPage <= currentPage) {
             $(".loadmore .loading").show().addClass("success");
-            $(".loadmore .message").html("Thats all!");
+            $(".loadmore .message").html("All Results Displayed");
             $(".loadmore img").hide();
             allowtoLoad = false
         } else {
