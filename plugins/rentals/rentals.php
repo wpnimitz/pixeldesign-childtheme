9e69eb36129b21a93c4088eb7f1b13ac4e5e0ea6
dougintexas <doug@pixelprimedesign.com>
2020-01-07 13:05:22 -0600

    Revert "comment removal and update map"
    This reverts commit c905e84f7c6accf8a85cdbe90f8ad9ec36932771.

--- plugins/rentals/rentals.php
+++ plugins/rentals/rentals.php
@@ -254,7 +254,7 @@ function show_rental_marker() {
 
 	$ret .= '<div class="rental_mapper">';
 
-		$ret .= '<img src="'. get_stylesheet_directory_uri() .'/assets/img/MasterPlanCommunityMap_ForWebsiteV2-1.jpg" width="100%" class="pin" easypin-id="rental_map">';
+		$ret .= '<img src="'. get_stylesheet_directory_uri() .'/assets/img/delmar_map.jpg" width="100%" class="pin" easypin-id="rental_map">';
 		$ret .= '<marker style="display:none"><img data-coor="'. $rental_coordinates. '" src="'. get_stylesheet_directory_uri() .'/assets/svg/MapPointer.svg" class="map-canvass"></marker>';
 
 		
@@ -813,7 +813,7 @@ function rental_metabox_callback( $meta_id ) {
 
 	echo '<div class="mapper-defaults" style="display:none;">';
 		echo '<img src="'. get_stylesheet_directory_uri() .'/assets/svg/MapPointer.svg" class="pin-marker">';
-		echo '<img src="'. get_stylesheet_directory_uri() .'/assets/img/MasterPlanCommunityMap_ForWebsiteV2-1.jpg" class="map-canvass">';
+		echo '<img src="'. get_stylesheet_directory_uri() .'/assets/img/delmar_map.jpg" class="map-canvass">';
 	echo '</div>';
 
 	$rental_coordinates = get_post_meta( $meta_id->ID, 'rental_coordinates', true );
@@ -823,7 +823,7 @@ function rental_metabox_callback( $meta_id ) {
 	}
 	echo '<div class="rental_mapper">';
 
-		echo '<img src="'. get_stylesheet_directory_uri() .'/assets/img/MasterPlanCommunityMap_ForWebsiteV2-1.jpg" width="800" class="pin" easypin-id="rental_map">';
+		echo '<img src="'. get_stylesheet_directory_uri() .'/assets/img/delmar_map.jpg" width="800" class="pin" easypin-id="rental_map">';
 		echo '<marker style="position: absolute; top: 30px; left: 30px; width: 30px;" class="drag"><img data-coor="'. $rental_coordinates. '" src="'. get_stylesheet_directory_uri() .'/assets/svg/MapPointer.svg" class="map-canvass"></marker>';
 
 		
